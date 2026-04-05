96068461c4483e37065d8ebd536e7ac034f27710
MaximeJR <m.bracq@ch-calais.fr>
2026-04-05 21:47:37 +0200

    v4

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE = "vitatrack-v4";
-const ASSETS = [
-  "/vitatrack/",
-  "/vitatrack/index.html",
-  "/vitatrack/manifest.json",
-  "/vitatrack/icon-192.svg",
-  "/vitatrack/icon-512.svg"
-];
+const CACHE = "vitatrack-v5";
+const ASSETS = ["/vitatrack/", "/vitatrack/index.html", "/vitatrack/manifest.json", "/vitatrack/icon-192.svg", "/vitatrack/icon-512.svg"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
