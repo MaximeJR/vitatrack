8b45f2e3df1279e33a98b7fb5da02f86ad9f2396
MaximeJR <m.bracq@ch-calais.fr>
2026-04-05 21:18:32 +0200

    fix des url

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE = "vitatrack-v4";
-const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.svg", "/icon-512.svg"];
+const ASSETS = [
+  "/vitatrack/",
+  "/vitatrack/index.html",
+  "/vitatrack/manifest.json",
+  "/vitatrack/icon-192.svg",
+  "/vitatrack/icon-512.svg"
+];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
